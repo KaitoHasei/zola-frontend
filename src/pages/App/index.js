49b0a7e9d0dc9699d002b2e1d1526e4d5a54e079
CongLinh1003 <01633007559mc@gmail.com>
2024-04-08 21:22:45 +0700

    Create ConversationInfo folder, and import assets in RegisterSuccess instead of ussing path

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -17,9 +17,6 @@ import {
   Text,
   IconButton,
   HStack,
-  VStack,
-  Button,
-  StackDivider,
 } from "@chakra-ui/react";
 import { Icon } from "@iconify-icon/react";
 import _ from "lodash";
@@ -36,7 +33,7 @@ import Message from "#/components/Message";
 import "./App.scss";
 import { SocketContext } from "#/contexts/SocketContext";
 import PreviewImageUpload from "#/components/PreviewImageUpload";
-
+import ConversationInfo from "./ConversationInfo";
 function App() {
   const { user, conversationId } = useContext(GlobalContext);
   const { socket, setSocket } = useContext(SocketContext);
@@ -286,159 +283,6 @@ function App() {
     handleRemoveImage,
   ]);
 
-  const [activeView, setActiveView] = useState("default");
-
-  const handleViewChange = (view) => {
-    setActiveView(view);
-  };
-  const renderConversationInfo = useMemo(() => {
-    return (
-      <VStack
-        width="30%"
-        height="100%"
-        padding="1rem 0"
-        borderLeft="solid lightgrey 1px"
-      >
-        {activeView === "default" && (
-          <VStack width={"100%"}>
-            <Box
-              width="100%"
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-              alignItems="center"
-            >
-              <Avatar
-                size="xl"
-                src={getConversationAvatar(conversation, user.id)}
-                bg="gray.400"
-              />
-              <Text fontSize="lg" fontWeight="bold" marginTop="2">
-                {formatConversationName(conversation, user.id)}
-              </Text>
-            </Box>
-            <VStack
-              padding="0.5rem 0"
-              borderTop="solid lightgrey 1px"
-              divider={<StackDivider borderColor="gray.200" />}
-              width="100%"
-            >
-              <Button
-                onClick={() => handleViewChange("media")}
-                leftIcon={<Icon icon="pajamas:media" />}
-                justifyContent="flex-start"
-                width="100%"
-                variant="ghost"
-                color="black"
-                _hover={{ bg: "#ebedf0" }}
-              >
-                Media
-              </Button>
-              <Button
-                onClick={() => handleViewChange("file")}
-                leftIcon={<Icon icon="mdi:files" />}
-                justifyContent="flex-start"
-                width="100%"
-                variant="ghost"
-                color="black"
-                _hover={{ bg: "#ebedf0" }}
-              >
-                Files
-              </Button>
-              <Button
-                onClick={() => handleViewChange("link")}
-                leftIcon={<Icon icon="dashicons:admin-links" />}
-                justifyContent="flex-start"
-                width="100%"
-                variant="ghost"
-                color="black"
-                _hover={{ bg: "#ebedf0" }}
-              >
-                Links
-              </Button>
-            </VStack>
-          </VStack>
-        )}
-        ;
-        {activeView === "media" && (
-          <VStack width={"100%"}>
-            <HStack
-              position="relative"
-              display="flex"
-              justifyContent="center"
-              width="100%"
-              alignItems="center"
-            >
-              <Box
-                position="absolute"
-                left="0"
-                onClick={() => handleViewChange("default")}
-                padding="0.5rem"
-              >
-                <Icon icon="humbleicons:arrow-go-back" width="1.5rem" />
-              </Box>
-
-              <Text fontSize="lg" fontWeight="bold">
-                Media
-              </Text>
-            </HStack>
-          </VStack>
-        )}
-        ;
-        {activeView === "link" && (
-          <VStack width={"100%"}>
-            <HStack
-              position="relative"
-              display="flex"
-              justifyContent="center"
-              width="100%"
-              alignItems="center"
-            >
-              <Box
-                position="absolute"
-                left="0"
-                onClick={() => handleViewChange("default")}
-                padding="0.5rem"
-              >
-                <Icon icon="humbleicons:arrow-go-back" width="1.5rem" />
-              </Box>
-
-              <Text fontSize="lg" fontWeight="bold">
-                Links
-              </Text>
-            </HStack>
-          </VStack>
-        )}
-        ;
-        {activeView === "file" && (
-          <VStack width={"100%"}>
-            <HStack
-              position="relative"
-              display="flex"
-              justifyContent="center"
-              width="100%"
-              alignItems="center"
-            >
-              <Box
-                position="absolute"
-                left="0"
-                onClick={() => handleViewChange("default")}
-                padding="0.5rem"
-              >
-                <Icon icon="humbleicons:arrow-go-back" width="1.5rem" />
-              </Box>
-
-              <Text fontSize="lg" fontWeight="bold">
-                Files
-              </Text>
-            </HStack>
-          </VStack>
-        )}
-        ;
-      </VStack>
-    );
-  }, [user, conversation, activeView]);
-
   return (
     <>
       {!conversationId ? (
@@ -452,7 +296,12 @@ function App() {
           <FeedLayout height="100%" title={renderTitle} width="70%">
             {renderFeedChild}
           </FeedLayout>
-          {renderConversationInfo}
+          <ConversationInfo
+            user={user}
+            conversation={conversation}
+            getConversationAvatar={getConversationAvatar}
+            formatConversationName={formatConversationName}
+          />
         </HStack>
       )}
     </>
