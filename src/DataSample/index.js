e1d4ee82bf5961aac0d14958a808c5ba57603827
nhatlinh <nhatlinhdev201@gmail.com>
2024-04-10 16:24:25 +0700

    update

---
+++ src/DataSample/index.js
@@ -0,0 +1,50 @@
+export const DataSample = [
+  {
+    uid: 'SL03Q27AlyPq80kAVvTr6lB2zhA3',
+    displayName: 'Kaito Hasei',
+    email: 'kaitohasei@gmail.com',
+    bio: 'DHKTPM16A ok ok',
+    dob: '2002-01-20'
+  },
+  {
+    uid: 'INSlzOrq4wRTt2TUaYehond3W0E2',
+    displayName: 'A User',
+    email: 'a@a.com',
+  },
+  {
+    uid: 'VnrjJJv54ZbUVNYhJWclm0j03OM2',
+    displayName: 'kidie',
+    email: 'buitranthienan1111@gmail.com',
+    emailVerified: false,
+  },
+  {
+    uid: 'Y0sxZ3uee4YfI4fKrmNeBcwS3XT2',
+    displayName: 'B User',
+    email: 'b@a.com',
+    photoUrl: 'https://zola-bucket.s3.amazonaws.com/users/6613b1d06dcff849e22baa0a/avatar-1712576515450'
+  },
+  {
+    uid: 'SL03Q27AlyPq80kAVvTr6lB2zhA33',
+    displayName: 'Kaito Hasei',
+    email: 'kaitohasei@gmail.com',
+    bio: 'DHKTPM16A ok ok',
+    dob: '2002-01-20'
+  },
+  {
+    uid: 'INSlzOrq45wRTt2TUaYehond3W0E2',
+    displayName: 'A User',
+    email: 'a@a.com',
+  },
+  {
+    uid: 'VnrjJJv54Z4bUVNYhJWclm0j03OM2',
+    displayName: 'kidie',
+    email: 'buitranthienan1111@gmail.com',
+    emailVerified: false,
+  },
+  {
+    uid: 'Y0sxZ3uee4Y8fI4fKrmNeBcwS3XT2',
+    displayName: 'B User',
+    email: 'b@a.com',
+    photoUrl: 'https://zola-bucket.s3.amazonaws.com/users/6613b1d06dcff849e22baa0a/avatar-1712576515450'
+  }
+]
