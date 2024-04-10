7417865f32a2426de886b41db8c33412018f8186
nhatlinh <nhatlinhdev201@gmail.com>
2024-04-10 16:24:25 +0700

    update

--- src/pages/ListFriend/index.js
+++ src/pages/ListFriend/index.js
@@ -1,22 +1,232 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
+import CustomAlert from '#/CustomAlert';
+import { get, post } from '#/axios';
+import SearchFriendModal from '#/components/SearchFriendModel';
+import { Box, Flex, Text, Button, Input, Avatar } from '@chakra-ui/react'
 import { Icon } from '@iconify-icon/react'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useCallback } from 'react';
+import { GlobalContext } from '#/contexts/GlobalContext';
+import { useNavigate } from "react-router-dom";
 
 const ListFriend = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userList, setUserList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortBy, setSortBy] = useState('name');
+  const [data, setData] = useState([]);
+  const [alt, setAlt] = useState(false);
+  const [mess, setMess] = useState('');
+  const [statusMess, setStatusMess] = useState('info');
+  const navigate = useNavigate();
+
+  
+  const { user, setUser, setConversationId } = useContext(GlobalContext);
+
+
+  useEffect(() => {
+    getContacts();
+    getAllFriendUser();
+  }, []);
+
+  const getContacts = async () => {
+    try {
+      const response = await get('/contacts');
+      if (response.status === 200) {
+        setUserList(response.data);
+      }
+    } catch (error) {
+      console.log('Err get contacts list : ', error);
+    }
+  }
+
+  const getAllFriendUser = async () => {
+    try {
+      const response = await get("/contacts/get-friends");
+      if (response.status === 200) {
+        setData(response.data);
+        console.log("list friend : ", response.data);
+      }
+    } catch (error) {
+      console.log("error get list friend : ", error);
+    }
+  }
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+  const filteredData = data.filter(item => {
+    if (
+      item.friend.displayName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      item.friend.email.toLowerCase().includes(searchTerm.toLowerCase())
+    ) {
+      return true;
+    }
+    return false;
+  });
+
+  const sortedData = [...filteredData].sort((a, b) => {
+    if (sortBy === 'name') {
+      return a.friend.displayName.localeCompare(b.friend.displayName);
+    }
+    return 0;
+  });
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSortChange = (type) => {
+    setSortBy(type);
+  };
+
+  const handleGoChat =  useCallback(
+    async (id) => {
+      if (!user) return;
+
+      try {
+        const response = await post("/conversations", {
+          participantId: id,
+        });
+
+        if (response?.status === 201)
+          setConversationId(response?.data?.id);
+        return navigate("/");
+      } catch (error) { }
+    },
+    [setConversationId]
+  );
+
+  const handleRemoveFriend = async (id) => {
+    try{
+      const response = await post("/contacts/remove-friend", {id});
+      if(response.status === 200) {
+        setMess('Remove successfully !');
+        setStatusMess('success');
+        setAlt(true);
+        setTimeout(() => {
+          setAlt(false);
+        }, 1000);
+        getAllFriendUser();
+      } else {
+        setMess('Error remove friend !');
+        setStatusMess('error');
+        setAlt(true);
+        setTimeout(() => {
+          setAlt(false);
+        }, 1000);
+      }
+    } catch(error) {
+      console.log("Error remove friend : ", error);
+    }
+  }
+
+  const handleViewProfile = (id) => {
+
+  }
   return (
     <div style={{ height: '100vh' }}>
       {/* header */}
       <Box style={{
         borderBottom: '1px solid gray'
       }}>
-        <Flex align='center' h="60px" direction='row' color="black" p={5}>
-          <Icon icon="mingcute:contacts-line" width='40px' height='25px' />
-          <Text as='b'>Danh sách bạn bè</Text>
+        <Flex align='center' h="60px" direction='row' color="black" p={5} justifyContent='space-between'>
+          <Box display='flex'>
+            <Icon icon="mingcute:contacts-line" width='40px' height='25px' />
+            <Text>List friends</Text>
+          </Box>
+          <Box>
+            <Button onClick={handleOpenModal}><Icon icon="fluent-mdl2:add-friend" /></Button>
+          </Box>
+        </Flex>
+        <SearchFriendModal isOpen={isModalOpen} onClose={handleCloseModal} userList={userList} />
+      </Box >
+
+      <Box>
+        <Flex align="center" h="60px" direction="row" color="black">
+          <Icon icon="bytesize:search" style={{ color: '#fbf9f9' }} />
+          <Input
+            placeholder="Search by name or email"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            ml={2}
+            mr={4}
+          />
+          <Button
+            variant={sortBy === 'time' ? 'solid' : 'outline'}
+            onClick={() => handleSortChange('time')}
+            mr={2}
+          >Sort by time</Button>
+          <Button
+            variant={sortBy === 'name' ? 'solid' : 'outline'}
+            onClick={() => handleSortChange('name')}
+          >Sort by name</Button>
         </Flex>
       </Box>
+      <Box overflowY="scroll" height="calc(100vh - 120px)">
+        <Box bg='teal.20'>
+          <Flex bg='teal.20' direction="row">
+            {sortedData.length <= 0 ?
+              (<Box w='100%' p={5}>
+                <Flex justifyContent='center' alignContent='center' >
+                  <Text textAlign='center'>Not exists new requirement !</Text>
+                </Flex>
+              </Box>) :
+              (sortedData.map(item => (
+                <Box key={item.id} p={4} mt={4} borderRadius="lg" boxShadow="md" bg="white" w='100%'>
+                  <Flex justifyContent='space-between' alignItems='center' mx={4}>
+                    <Box>
+                      <Avatar src={item.friend.photoUrl} style={{ border: '1px solid #008080', maxWidth: '48px', maxHeight: '48px' }} />
+                    </Box>
+                    <Box flex="1" ml={2}>
+                      <Text fontSize="lg" fontWeight="bold">{item.friend.displayName}</Text>
+                      <Text fontSize="sm" fontStyle="italic">Email: {item.friend.email}</Text>
+                    </Box>
+                    <Flex>
+                      <Button
+                        aria-label="Chat"
+                        colorScheme="teal"
+                        onClick={() => handleGoChat(item.friend.id)} 
+                        mr={2}
+                      >Chat</Button>
+                      <Button
+                        aria-label="View Profile"
+                        colorScheme="teal"
+                        onClick={() => handleViewProfile(item.friend.id)} 
+                        mr={2}
+                      >View</Button>
+                      <Button
+                        aria-label="Delete Friend"
+                        colorScheme="red"
+                        onClick={() => handleRemoveFriend(item.id)}
+                      >Remove</Button>
+                    </Flex>
+                  </Flex>
+                </Box>
+              )))
+            }
+          </Flex>
+        </Box>
+        <Box px={3}
+          style={{
+            width: "100%",
+            height: '55px',
+            position: 'fixed',
+            bottom: 0,
+            right: '10px',
+            zIndex: '9999'
+          }}>
+          {alt ? (<CustomAlert message={mess} status={statusMess} style={{ position: 'fixed', bottom: '10px', right: '10px', zIndex: '9999' }} />) : null}
+        </Box>
+      </Box>
+
       {/* main scroll */}
-      <Box overflowY="scroll" height="calc(100vh - 60px)">
+      <Box Box overflowY="scroll" height="calc(100vh - 60px)" >
         <Flex bg='teal.20'>
+
 
         </Flex>
       </Box>
